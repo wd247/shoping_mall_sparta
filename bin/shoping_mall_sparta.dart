4be4ecb8b61c03fec4c19d2d01f1b57a1bcab85f
wd247 <junny3344@gmail.com>
2024-11-12 16:51:56 +0900

    스파르타버전

---
+++ bin/shoping_mall_sparta.dart
@@ -0,0 +1,5 @@
+import 'package:shoping_mall_sparta/shoping_mall_sparta.dart' as shoping_mall_sparta;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shoping_mall_sparta.calculate()}!');
+}
---
+++ test/shoping_mall_sparta_test.dart
@@ -0,0 +1,8 @@
+import 'package:shoping_mall_sparta/shoping_mall_sparta.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
